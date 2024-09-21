59a64e9e5d8e4040df88d549feef85b58ad2941e
bellebelle0 <bellebelle0@gmail.com>
2024-09-21 21:02:07 +0800

    update main.py

--- assignment/src/main.py
+++ assignment/src/main.py
@@ -22,62 +22,55 @@ tree = parse(page, 'html')
 
 # get tables from the desired sections
 tables = tree.xpath(os.getenv('TABLE_XPATH'))
-tables = tables[3:18]
+tables = tables[2:17]
+# print('Total tables: ', len(tables))
 
 #get data in table
 table_contents = [table.text_content() for table in tables]
 
 #global variables to hold data
+year_data = []
 dog_data = []
 
 #access data in table rows
 for table in tables:
-    # TODO: get other rows
+    # TODO: get other animal rows
     dog_row = table.xpath(os.getenv('DOG_ROW_XPATH'))
-    for item in dog_row:
-        # print(item)
-        # print(str(item))
-        dog_row_contents = item.xpath(os.getenv('ROW_DATA_XPATH'))
-        for content in dog_row_contents:
-            data = str(content.text_content()).strip()
-            if data == "Dogs":
-                continue
-            else:
-                dog_data.append(data)
-            print(data)
-            
-#TODO: plot dog data
-print(dog_data)
-#create plot
+    # dog_alt_row = table.xpath(os.getenv('DOG_ROW_ALT_XPATH'))
+    
+    # access each column in the dog rows
+    for col in dog_row:
+        dog_row_contents = col.xpath(os.getenv('ROW_DATA_XPATH'))
+
+        #tables have varying structures so check for empty rows
+        if not len(dog_row_contents) == 0:
+            #get year data
+            year_header = table.xpath(os.getenv('YEAR_XPATH'))
+            for header in year_header:
+                year_string = str(header.text_content())[:-5]
+            year_data.append(year_string.removeprefix('Fiscal Year ')) 
+
+            #get dog data
+            for content in dog_row_contents:
+                data = str(content.text_content()).strip()
+                # print(data)
+                if data == "Dogs":
+                    continue
+                elif '%' in data:
+                    continue
+                else:
+                    dog_data.append(data)
+
+#get the "Animals In" data from the row
+dogs_in = []
+for stat in range(0, len(dog_data), 3):
+    dogs_in.append(dog_data[stat])
+
+# create and show plot
 fig, ax = plt.subplots()
+data_len = len(year_data)
+# remember to convert data from string to int
+ax.plot([int(year_data[data]) for data in range(data_len)], [int(dogs_in[data]) for data in range(data_len)])
 plt.show()
 
-# print the rows
-# row_num = 3
-# for row in rows:
-#     columns = row.xpath(os.getenv('COL_XPATH'))
-#     columns = [column.text_content() for column in columns]
-#     columns = [column.strip() for column in columns]
-#     row_string = " ".join(columns).strip()
-
-#     # skip empty rows
-#     if row_string.strip() == "":
-#         continue
-
-#     row_num += 1
-
-#     print(f'Row {row_num}: {row_string}')
-
-#     month = int(columns[0])
-#     day = int(columns[1])
-            
-#     for i in range(2, len(columns), 2):
-#         if columns[i] != "":
-#             # get the time in HHMM format
-#             hour = columns[i][:2]
-#             minute = columns[i][2:]
-
-#             dt = datetime.datetime(year,month,day,int(hour),int(minute))
-#             value = columns[i+1]
-#             print(f'{dt} - {value}')            
 
