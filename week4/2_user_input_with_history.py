b941c8d3db0efdf8902a35343bfe9e7020ec8a30
bellebelle0 <bellebelle0@gmail.com>
2024-10-07 10:33:59 +0800

    week 4 comments

--- week4/2_user_input_with_history.py
+++ week4/2_user_input_with_history.py
@@ -1,13 +1,19 @@
 import streamlit as st
 
+# st.session_state is a dict that stores info for the length of the session
+# if key "messages" not in session state
+# messages is self-defined
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
+# writes message to page
 for msg in st.session_state.messages:
+    # chat_message creates the bubble with param name
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
+    print(st.session_state.messages)
     
--- week4/3_chat_with_response.py
+++ week4/3_chat_with_response.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
+# if no prior message or no message history
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
+# if receive input, make prompt equal the inpit
 if prompt := st.chat_input():
 
+    # add input to message history and display
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
