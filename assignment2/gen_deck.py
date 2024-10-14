26f9a64c8d987c7e8332b8609d689f723fd74de0
bellebelle0 <bellebelle0@gmail.com>
2024-10-14 22:43:16 +0800

    update assignment2

--- assignment2/gen_deck.py
+++ assignment2/gen_deck.py
@@ -1,26 +1,62 @@
+# messages are taken from: https://labyrinthos.co/blogs/learn-tarot-with-labyrinthos-academy/how-to-calculate-your-tarot-birth-card-plus-short-birth-card-meanings-infographic?srsltid=AfmBOorHPTalNfpdN2_On6USAQoHp0Uumn_mbd7rufxllXn8IXA-FU65
+
 import json
 
-arcana_nums = [x for x in range(22)]
-arcanas = ["The Fool",
-           "The Magician",
-           "The High Priestess",
-           "The Empress",
-           "The Emperor",
-           "The Hierophant",
-           "The Lovers",
-           "The Chariot",
-           "Strength",
-           "The Hermit",
-           "Wheel of Fortune",
-           "Justice",
-           "The Hanged Man",
-           "Death",
-           "Temperance",
-           "The Devil",
-           "The Tower",
-           "The Star",
-           "The Moon",
-           "The Sun",
-           "The Last Judgment",
-           "The World"]
+birth_card_map = {
+    10: "Wheel of Fortune/Magician",
+    11: "Justice/High Priestess",
+    12: "Hanged Man/Empress",
+    13: "Death/Emperor",
+    14: "Temperance/Hierophant",
+    15: "Devil/Lovers",
+    16: "Tower/Chariot",
+    17: "Star/Strength",
+    18: "Moon/Hermit",
+    19: "Sun/Wheel of Fortune/Magician",
+    20: "Judgement/High Priestess",
+    21: "World/Empress"
+}
 
+msg_map = {
+    10: "These birth cards deal with following the cyclic change of the universe and the random hand of fate (The Wheel of Fortune) using the manifestation powers and resourcefulness of the Magician. Wherever the wheel shifts, you will be ready.",
+    11: "Your path unites clarity, fairness and harmony with deep intuitive understanding and knowledge. Justice and the High Priestess are concerned with two different sides in life - the transactional and the spiritual. Your insights tend to go deeply, a powerful compliment with Justice’s objectivity and sharp decision making.",
+    12: "This path represents gentle progress during moments of delays, and the willful sacrifices of the Hanged Man made with the nurturing care of the Empress. When faced with the Hanged Man’s deliberate choice to remain in his suspension, the love within the empress guides him to continue his spiritual path.",
+    13: "In this path, the Death card represents the ending of cycles - of closing doors to open others anew. It is here where the Emperor’s power, authority and structure are needed in order to create stability and security. For the new cycle to be ushered in, a strong and willful hand will be needed to steer its movement.",
+    14: "Your path is one of guidance - of using your great wisdom (The Hierophant) to bring calm and peace to conflict (Temperance). Having these two cards come together is akin to having two great advisors - one bearing the keys to long established knowledge, and the other equipped with the gentle serenity to unify disparate groups.",
+    15: "Deep sensuality is indicated by this birth card pair - for the Devil is the combination of both the divine and the beastly. He is playful in the realm of the material, but his pure enjoyment can lead some to be enslaved. The lovers however bring an element of love to his passionate animalistic nature. His beauty in the horror is redeemed.",
+    16: "Yours is the path of restoring balance in the face of violent transformation. In the face of groundbreaking change, it is your sense of balance that will be your guide. The Tower destroys foundations and structure ruthlessly in order to reveal a new path forward, but this energy will be guided by the Chariot’s deliberate and careful control of impulses.",
+    17: "This path is one of hope - of the choice to look at all that life brings you with optimism (The Star), requiring of you great inner Strength. Yours is the choice of moving away from sorrow, towards regeneration, which takes great resilience in times of suffering. To have hope is the source of your action, to have Strength is assurance that you will succeed.",
+    18: "Your path is about finding the clarity in the darkness, about finding truth in an uncertain world. The Moon presents us with phantoms, dreams and illusions. It is about the hidden facets that are just below the surface. The hermit is the seeker, who uses their internal truth to light the way to finding your own personal wisdom.",
+    19: "Like the Wheel and Magician birth cards, this path is about adapting to change, and moving alongside nature’s cycles resourcefully. It is about recognizing and understanding the natural shifts of the universe (The Wheel of Fortune) while using your inner powers of manifestation and creation to move along with those cycles with grace and success (The Magician). The Sun gives an air of contentment, joy and peace to this path, giving you the cue to enjoy the abundance that your path brings you.",
+    20: "This path is one of breaking limitations, of releasing oneself from established modes of thought, and rebuilding them anew (Judgement). You will be guided by your intuition (The High Priestess), and it is through learning to trust your inner voice that you will create your most important work.",
+    21: "Your path is one of self-actualization, to bring together all the disparate parts of yourself in order to feel completely at home in this world (The World). Throughout this path, you will be guided by love - both giving and receiving love (The Empress). This loving and caring aspect within you will be your compass towards fulfillment, and is the gift you give to all others."
+}
+
+birth_num_map = {}
+for i in range(10,22):
+    added_nums = []
+    added_nums.append(str(i))
+    #add down the two digits into one
+    single = int(str(i)[0]) + int(str(i)[1])
+    added_nums.append("0" + str(single))
+    if i == 19:
+        #19 will add down once more
+        added_nums.append("01")
+
+    #add final result to map
+    birth_num_map[i] = added_nums
+
+result = {}
+for i in range(10, 22):
+    result[i] = {
+        "card_pair":birth_card_map[i],
+        "card_num": birth_num_map[i],
+        "message": msg_map[i]
+    }
+
+output_path = "tarot_results.json"
+with open(output_path, 'w') as f:
+    data = json.dumps(result)
+    f.write(data)
+
+f.close()
--- assignment2/interactive.py
+++ assignment2/interactive.py
@@ -1,38 +1,188 @@
 # tarot card dataset : https://github.com/metabismuth/tarot-json/releases/tag/v0
+import json
+import cv2
+import numpy as np
+import asyncio
+import textwrap
 
-# TODO: map a value to tartot card names and lifepath desc
-#TODO: function to add birthdates
+# map for lifepath sums that are a single digit
+digit_map = {
+    1: 10,
+    2: 11,
+    3: 12,
+    4: 13,
+    5: 14,
+    6: 15,
+    7: 16,
+    8: 17,
+    9: 18
+}
+
+# load data from json with possible results
+with open('tarot_results.json', 'r') as f:
+    data = json.load(f)
+
+# calculate lifepath number based on birthdate
 def calculateLifePath(birthDate):
-    #split birthdays by every two digits
-    #add digits
-    #check if final number is within available tarot values
-    #if not reduce again
-    #else return value
-    #return secondary values
-    pass
+    #split birthday by two digits and add
+    month = int(birthDate[0]) + int(birthDate[1])
+    day = int(birthDate[2]) + int(birthDate[3])
+    year1 = int(birthDate[4]) + int(birthDate[5])
+    year2 = int(birthDate[6]) + int(birthDate[7])
 
-def getCard(lifePath):
-    #use map to get tarot card and desc
-    pass
+    sum1 = month + day + year1 + year2
 
-def showResult():
-    #show the card and its desc in a popup window
-    pass
+    #reduce first sum to a 2 digit number if 3 digits
+    sum2 = None
+    if len(str(sum1)) == 3:
+        sum2 = int(str(sum1)[0:2]) + int(str(sum1)[-1])
+    elif len(str(sum1)) == 2:
+        #if 2 digits, reduce if over 21 (major arcana only go up to 21)
+        if not sum1 <= 21:
+            sum2 = int(str(sum1)[0]) + int(str(sum1)[1])
+        else:
+            sum2 = sum1    
+    else:
+        # if single digit, do nothing
+        sum2 = sum1
 
+    # second sum reduction
+    sum3 = None
+    if len(str(sum2)) == 2:
+        if not sum2 <= 21:
+            sum3 = int(str(sum2)[0]) + int(str(sum2)[1])
+        else:
+            sum3 = sum2
+    else:
+        sum3 = sum2
 
-#TODO: Main
-if __name__ == '__main__':
+    # redirect single digit sums to corresponding birth card pair
+    if len(str(sum3)) == 1:
+        final_sum = digit_map[sum3]
+    else:
+        final_sum = sum3
+
+    return final_sum
+
+async def showResult(lifePath):
+    # get result data
+    result = data[str(lifePath)]
+    
+    #get filepath to card images
+    card_images = []
+    for num in range(len(result["card_num"])):
+        filepath = "deck_images/m" + result["card_num"][num] + ".jpg"
+        print(filepath)
+        card_images.append(cv2.imread(filepath))
+
+    #isplay result
+    # get result information to display
+    result_title = result["card_pair"]
+    result_msg = textwrap.wrap(result["message"], width=35) #wrap message to fit in window
+
+    # create result display window
+    window_width = 2000
+    window_height = 1000
+    label_height = round(window_height/8)*2
+    width_margin = round(window_width/24)
+    output_display = np.ones((window_height,window_width,3), np.uint8)*225
+
+    # add result title to display window
+    cv2.putText(output_display, result_title, 
+                (64, 148), cv2.FONT_HERSHEY_SIMPLEX, 2, (42, 42, 165), 3,cv2.LINE_AA, bottomLeftOrigin=False)
+    
+    # display first card
+    card1 = {
+        "x1": width_margin,
+        "x2": card_images[0].shape[1]+width_margin
+    }
+    output_display[label_height:card_images[0].shape[0]+label_height, card1["x1"]:card1["x2"]] = card_images[0]
+
+    # display second card
+    card2 = {
+        "x1": card1["x2"] + 24,
+        "x2": card1["x2"] + 24 + card_images[1].shape[1]
+    }
+    output_display[label_height:card_images[1].shape[0]+label_height, card2["x1"]:card2["x2"]] = card_images[1]
+
+    #display third card, if any
+    if len(card_images) == 3:
+        card3 = {
+            "x1": card2["x2"] + 24,
+            "x2": card2["x2"] + 24 + card_images[2].shape[1]
+        }
+        output_display[label_height:card_images[2].shape[0]+label_height, card3["x1"]:card3["x2"]] = card_images[2]
+
+        #add message text, wrappedtext is not string so modify display window np array
+        for i, line in enumerate(result_msg):
+            textsize = cv2.getTextSize(line, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+
+            gap = textsize[1] + 14
+
+            y = int((label_height + textsize[1])) + i * gap
+            x = int(((output_display.shape[1]-width_margin) - textsize[0]))
+
+            cv2.putText(output_display, line, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, 
+                        (42, 42, 165), 
+                        2, 
+                        lineType = cv2.LINE_AA)
+    else:
+        #add message text, wrappedtext is not string so modify display window np array
+        for i, line in enumerate(result_msg):
+            textsize = cv2.getTextSize(line, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+
+            gap = textsize[1] + 14
+
+            y = int((label_height + textsize[1])) + i * gap
+            x = int(((output_display.shape[1]-width_margin) - textsize[0]))
+
+            cv2.putText(output_display, line, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, 
+                        (42, 42, 165), 
+                        2, 
+                        lineType = cv2.LINE_AA)
+
+    
+    
+    # show result in a separate window
     while True:
-        print('Please input your birthday in the form DDMMYY. For example: January 1st, 1999 would be 01011999')
+        cv2.imshow("Result", output_display)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+async def main():
+    while True:
+        print('Please input your birthday in the form MMDDYY. For example: January 2nd, 1999 would be 01021999')
         birthDate = input()
 
+        # ensure that input is the correct length
+        if not len(birthDate) == 8:
+            print('Incorrect format inputted, please try again.')
+            continue
+
+        #check that the input is correct format
+        try:
+            int(birthDate)
+        except:
+            print('Incorrect format inputted, please try again.')
+            continue
+
+        # calculate and show result
+        lifePath = calculateLifePath(birthDate)
+        await showResult(lifePath)
+
+        # after first result is quit, ask user if they want to try again
         restart = input('Ask another birthday? y/n \n')
         if restart.lower() == 'y':
             continue
         else:
             break
 
+if __name__ == '__main__':
+    #run program
+    asyncio.run(main())
 
-#return corresponding card and display it
 
 #TODO: add keyword inputs such as favorite animal to customize a tarot card?
